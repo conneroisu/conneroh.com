03b27de1d1d071e537e707edd11569c2ec01d1d8
connerohnesorge <conneroisu@outlook.com>
2025-02-27 08:17:59 -0600

    latest

--- internal/data/master/schemas/tag.sql
+++ internal/data/master/schemas/tag.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS tags (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL UNIQUE,
     description TEXT NOT NULL,
-    slug TEXT NOT NULL,
+    slug TEXT NOT NULL UNIQUE,
     icon TEXT NOT NULL DEFAULT 'tag',
     classes TEXT NOT NULL DEFAULT 'uk-badge-secondary',
     created_at INTEGER NOT NULL DEFAULT (unixepoch('now')),
