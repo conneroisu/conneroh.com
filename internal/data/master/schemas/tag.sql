57c864595768f9a2feb67254f9aae5f030d09005
connerohnesorge <conneroisu@outlook.com>
2025-02-27 09:16:17 -0600

    latest

--- internal/data/master/schemas/tag.sql
+++ internal/data/master/schemas/tag.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS tags (
     name TEXT NOT NULL UNIQUE,
     description TEXT NOT NULL,
     slug TEXT NOT NULL UNIQUE,
-    icon TEXT NOT NULL DEFAULT 'tag',
+    icon TEXT NOT NULL DEFAULT 'nf-fa-tag',
     classes TEXT NOT NULL DEFAULT 'uk-badge-secondary',
     created_at INTEGER NOT NULL DEFAULT (unixepoch('now')),
     updated_at INTEGER NOT NULL DEFAULT (unixepoch('now'))
