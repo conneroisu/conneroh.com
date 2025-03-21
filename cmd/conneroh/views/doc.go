9226069fcd52d46afc9540ce58584b4a39b66637
connerohnesorge <conneroisu@outlook.com>
2025-03-21 17:08:58 -0500

    latest

--- cmd/conneroh/views/doc.go
+++
@@ -1,4 +0,0 @@
-// Package views contains the HTML templates for the website.
-package views
-
-//go:generate gomarkdoc -o README.md -e .
--- cmd/conneroh/views/utils.go
+++ cmd/conneroh/views/utils.go
@@ -1,4 +1,7 @@
+// Package views contains the HTML templates for the website.
 package views
+
+//go:generate gomarkdoc -o README.md -e .
 
 import (
 	"fmt"
