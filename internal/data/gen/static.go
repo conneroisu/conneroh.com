4c911906c3ca90d268041a5e18f651a4219d96ac
connerohnesorge <conneroisu@outlook.com>
2025-03-26 14:21:30 -0500

    latest

--- internal/data/gen/static.go
+++ internal/data/gen/static.go
@@ -27,32 +27,33 @@ type (
 		Embedded
 	}
 
-	// Embedded is a struct with a large number of float64 fields.
+	// Embedded is a base struct for all embedded structs.
 	Embedded struct {
-		Title        string               `yaml:"title"`
-		Slug         string               `yaml:"slug"`
-		Description  string               `yaml:"description"`
-		Content      string               `yaml:"-"`
-		BannerPath   string               `yaml:"banner_path"`
-		RawContent   string               `yaml:"-"`
-		Icon         string               `yaml:"icon"`
-		CreatedAt    time.Time            `yaml:"created_at"`
-		UpdatedAt    time.Time            `yaml:"updated_at"`
-		X            float64              `yaml:"-"`
-		Y            float64              `yaml:"-"`
-		Z            float64              `yaml:"-"`
-		Vec          [EmbedLength]float64 `yaml:"-"`
-		TagSlugs     []string             `yaml:"tags"`
-		PostSlugs    []string             `yaml:"posts"`
-		ProjectSlugs []string             `yaml:"projects"`
-		Posts        []*Post              `yaml:"-" structgen:"PostSlugs"`
-		Tags         []*Tag               `yaml:"-" structgen:"TagSlugs"`
-		Projects     []*Project           `yaml:"-" structgen:"ProjectSlugs"`
+		Title           string               `yaml:"title"`
+		Slug            string               `yaml:"slug"`
+		Description     string               `yaml:"description"`
+		Content         string               `yaml:"-"`
+		BannerPath      string               `yaml:"banner_path"`
+		RawContent      string               `yaml:"-"`
+		Icon            string               `yaml:"icon"`
+		CreatedAt       time.Time            `yaml:"created_at"`
+		UpdatedAt       time.Time            `yaml:"updated_at"`
+		X               float64              `yaml:"-"`
+		Y               float64              `yaml:"-"`
+		Z               float64              `yaml:"-"`
+		Vec             [EmbedLength]float64 `yaml:"-"`
+		TagSlugs        []string             `yaml:"tags"`
+		PostSlugs       []string             `yaml:"posts"`
+		ProjectSlugs    []string             `yaml:"projects"`
+		EmploymentSlugs []string             `yaml:"employments"`
+		Posts           []*Post              `yaml:"-" structgen:"PostSlugs"`
+		Tags            []*Tag               `yaml:"-" structgen:"TagSlugs"`
+		Projects        []*Project           `yaml:"-" structgen:"ProjectSlugs"`
+		Employments     []*Employment        `yaml:"-" structgen:"EmploymentSlugs"`
 	}
 )
 
 // New creates a new instance of the given type.
-// IT IS KNOWN TO BE INEFFICIENT.
 func New[
 	T Post | Project | Tag,
 ](emb Embedded) *T {
