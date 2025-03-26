d7ee3b09e5d061b5d4e5301b3d4c78f4168e3ea4
connerohnesorge <conneroisu@outlook.com>
2025-03-26 03:27:36 -0500

    latest

--- internal/routing/main.go
+++ internal/routing/main.go
@@ -10,6 +10,32 @@ import (
 	"github.com/conneroisu/conneroh.com/internal/data/master"
 )
 
+// SingleTarget is the target of a single view.
+// string
+type SingleTarget = string
+
+const (
+	// SingleTargetPost is the target of a single post view.
+	SingleTargetPost SingleTarget = "post"
+	// SingleTargetProject is the target of a single project view.
+	SingleTargetProject SingleTarget = "project"
+	// SingleTargetTag is the target of a single tag view.
+	SingleTargetTag SingleTarget = "tag"
+)
+
+// PluralTarget is the target of a plural view.
+// string
+type PluralTarget string
+
+const (
+	// PluralTargetPost is the target of a plural post view.
+	PluralTargetPost PluralTarget = "posts"
+	// PluralTargetProject is the target of a plural project view.
+	PluralTargetProject PluralTarget = "projects"
+	// PluralTargetTag is the target of a plural tag view.
+	PluralTargetTag PluralTarget = "tags"
+)
+
 // FullFn is a function that handles a full request.
 type FullFn func(
 	fullPosts *[]master.FullPost,
@@ -77,29 +103,3 @@ func (m APIMap) AddRoutes(
 
 	return nil
 }
-
-// SingleTarget is the target of a single view.
-// string
-type SingleTarget = string
-
-const (
-	// SingleTargetPost is the target of a single post view.
-	SingleTargetPost SingleTarget = "post"
-	// SingleTargetProject is the target of a single project view.
-	SingleTargetProject SingleTarget = "project"
-	// SingleTargetTag is the target of a single tag view.
-	SingleTargetTag SingleTarget = "tag"
-)
-
-// PluralTarget is the target of a plural view.
-// string
-type PluralTarget string
-
-const (
-	// PluralTargetPost is the target of a plural post view.
-	PluralTargetPost PluralTarget = "posts"
-	// PluralTargetProject is the target of a plural project view.
-	PluralTargetProject PluralTarget = "projects"
-	// PluralTargetTag is the target of a plural tag view.
-	PluralTargetTag PluralTarget = "tags"
-)
