a77cac581c935e112b7d747c875a43099623c5ec
connerohnesorge <conneroisu@outlook.com>
2025-04-03 18:03:00 -0500

    latest

--- flake.nix
+++ flake.nix
@@ -25,18 +25,8 @@
   };
 
   nixConfig = {
-    extra-substituters = ''
-      https://cache.nixos.org
-      https://nix-community.cachix.org
-      https://devenv.cachix.org
-      https://conneroisu.cachix.org
-    '';
-    extra-trusted-public-keys = ''
-      cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
-      nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
-      devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
-      conneroisu.cachix.org-1:PgOlJ8/5i/XBz2HhKZIYBSxNiyzalr1B/63T74lRcU0=
-    '';
+    extra-substituters = ''https://conneroisu.cachix.org'';
+    extra-trusted-public-keys = ''conneroisu.cachix.org-1:PgOlJ8/5i/XBz2HhKZIYBSxNiyzalr1B/63T74lRcU0='';
     extra-experimental-features = "nix-command flakes";
   };
 
