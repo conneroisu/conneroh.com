bce616060c395c355af812ba81fe6cc69ca0d456
connerohnesorge <conneroisu@outlook.com>
2025-04-10 11:00:48 -0500

    latest

--- cmd/conneroh/routes.go
+++ cmd/conneroh/routes.go
@@ -36,12 +36,11 @@ func AddRoutes(
 		"GET /favicon.ico",
 		func(w http.ResponseWriter, _ *http.Request) {
 			_, _ = w.Write(static.Favicon)
-		},
-	)
+		})
 	h.HandleFunc(
 		"GET /api/search",
-		globalSearchHandler(gen.AllPosts, gen.AllProjects, gen.AllTags),
-	)
+		globalSearchHandler(gen.AllPosts, gen.AllProjects, gen.AllTags))
+
 	h.Handle(
 		"GET /posts",
 		templ.Handler(layouts.Page(posts)))
