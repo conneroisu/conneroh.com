c4027591cbba8f7a69733921aec77f86df38a8b2
connerohnesorge <conneroisu@outlook.com>
2025-05-15 15:33:17 -0500

    latest

--- nix/packages/devcontainer.nix
+++ nix/packages/devcontainer.nix
@@ -17,32 +17,34 @@ in {
       sha256 = "sha256-o4GvFCq6pvzASvlI5BLnk+Y4UN6qKL2dowuT0cp8q7Q=";
     };
   in {
-    packages.x86_64-linux.devcontainer = nix2container.buildImage {
-      name = "devcontainer";
-      initializeNixDatabase = true;
-      fromImage = alpine;
-      layers = [
-        (nix2container.buildLayer {
-          deps = [
-            pkgs.direnv
-          ];
-        })
-        (nix2container.buildLayer {
-          deps = [
-            self.devShells.${system}.default
-          ];
-        })
-      ];
-      config = {
-        Env = [
-          "NIX_PAGER=cat"
-          # A user is required by nix
-          # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
-          "USER=nobody"
-          # When running in podman on the GitHub CI, Nix fails to find the
-          # user home dir for an unkonwn reason...
-          "HOME=/"
+    packages.x86_64-linux = {
+      devcontainer = nix2container.buildImage {
+        name = "devcontainer";
+        initializeNixDatabase = true;
+        fromImage = alpine;
+        layers = [
+          (nix2container.buildLayer {
+            deps = [
+              pkgs.direnv
+            ];
+          })
+          (nix2container.buildLayer {
+            deps = [
+              self.devShells.${system}.default
+            ];
+          })
         ];
+        config = {
+          Env = [
+            "NIX_PAGER=cat"
+            # A user is required by nix
+            # https://github.com/NixOS/nix/blob/9348f9291e5d9e4ba3c4347ea1b235640f54fd79/src/libutil/util.cc#L478
+            "USER=nobody"
+            # When running in podman on the GitHub CI, Nix fails to find the
+            # user home dir for an unkonwn reason...
+            "HOME=/"
+          ];
+        };
       };
     };
   };
