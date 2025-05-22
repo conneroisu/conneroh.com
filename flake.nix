e1fe263b85e04f0dc692abc14821af3af8f13054
connerohnesorge <conneroisu@outlook.com>
2025-05-22 11:27:59 -0500

    fix deploy script

--- flake.nix
+++ flake.nix
@@ -295,12 +295,14 @@
 
                 [ -z "$arg" ] && { echo "No argument provided. Please provide a target environment. (dev or prod)"; exit 1; }
 
-                [ -z "$MASTER_FLY_AUTH_TOKEN" ] && MASTER_FLY_AUTH_TOKEN="$(doppler secrets get --plain MASTER_FLY_AUTH_TOKEN)"
-                TOKEN="$MASTER_FLY_AUTH_TOKEN"
                 if [ "$arg" = "dev" ]; then
+                  [ -z "$FLY_DEV_AUTH_TOKEN" ] && FLY_DEV_AUTH_TOKEN="$(doppler secrets get --plain FLY_DEV_AUTH_TOKEN)"
+                  TOKEN="$FLY_DEV_AUTH_TOKEN"
                   export FLY_NAME="conneroh-com-dev"
                   export CONFIG_FILE=${flyDevToml}
                 else
+                  [ -z "$FLY_AUTH_TOKEN" ] && FLY_AUTH_TOKEN="$(doppler secrets get --plain FLY_AUTH_TOKEN)"
+                  TOKEN="$FLY_AUTH_TOKEN"
                   export FLY_NAME="conneroh-com"
                   export CONFIG_FILE=${flyProdToml}
                 fi
