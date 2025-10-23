ba4f6a857e19798c8d06b2f0bac96abdafceab44
connerohnesorge <conneroisu@outlook.com>
2025-10-23 11:56:08 -0500

    add cms

---
+++ cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251023_155957 from './20251023_155957';
+
+export const migrations = [
+  {
+    up: migration_20251023_155957.up,
+    down: migration_20251023_155957.down,
+    name: '20251023_155957'
+  },
+];
